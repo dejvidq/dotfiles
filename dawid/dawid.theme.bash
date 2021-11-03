dc0b211db4f2cb4ec4e575c57182b119b6a761d9
Dawid Kotwica <dawid.kotwica2@globallogic.com>
2021-11-03 12:10:41 +0100

    Added venv name to PS1

--- dawid/dawid.theme.bash
+++ dawid/dawid.theme.bash
@@ -8,7 +8,15 @@ function short_path() {
    echo '$(echo $(echo $(dirname \w)|sed -e "s;\(/\.\?.\)[^/]*;\1;g" -e "s;/h/d;~;")/\W | sed -e "s;/h/~;~;" -e "s;///;/;;" -e "s;//;/;")'
 }
 
+function show_venv() {
+	if [[ -n "$VIRTUAL_ENV" ]]; then
+		echo "${orange}($(basename $VIRTUAL_ENV))${reset_color} "
+	else
+		echo ""
+	fi
+}
+
 function prompt_command() {
-	PS1="${green}$(short_path) ${cyan}$(git_info)${green}→ ${reset_color}"
+	PS1="$(show_venv)${green}$(short_path) ${cyan}$(git_info)${green}→ ${reset_color}"
 }
 safe_append_prompt_command prompt_command
