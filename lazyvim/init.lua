12c7fed1f676a98b556a1ba6767e82ffac229c87
dejvidq <15433565+dejvidq@users.noreply.github.com>
2023-04-22 13:26:03 +0200

    line number colors adjusted

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd([[highlight clear CursorLineNr]])
+vim.cmd([[highlight clear LineNr]])
+vim.cmd([[highlight LineNr guifg=white]])
+vim.cmd([[highlight CursorLineNr guifg=yellow]])
